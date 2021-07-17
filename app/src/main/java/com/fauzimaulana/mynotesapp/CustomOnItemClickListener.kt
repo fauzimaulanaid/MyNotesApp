76e57ff284a0b61a15470c9add1fb0e0c4de4123
fauzimaulanaid <muhammadfauzi1933@gmail.com>
2021-07-18 02:56:29 +0700

    Initial commit

---
+++ app/src/main/java/com/fauzimaulana/mynotesapp/CustomOnItemClickListener.kt
@@ -0,0 +1,13 @@
+package com.fauzimaulana.mynotesapp
+
+import android.view.View
+
+class CustomOnItemClickListener(private val position: Int, private val onItemClickCallback: OnItemClickCallback): View.OnClickListener {
+    interface OnItemClickCallback {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    override fun onClick(view: View) {
+        onItemClickCallback.onItemClick(view, position)
+    }
+}
---
+++ app/src/main/java/com/fauzimaulana/mynotesapp/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.fauzimaulana.mynotesapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.fauzimaulana.mynotesapp.adapter.NoteAdapter
+import com.fauzimaulana.mynotesapp.databinding.ActivityMainBinding
+import com.fauzimaulana.mynotesapp.db.NoteHelper
+import com.fauzimaulana.mynotesapp.entity.Note
+import com.fauzimaulana.mynotesapp.helper.MappingHelper
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: NoteAdapter
+
+    companion object {
+        private const val EXTRA_STATE = "EXTRA_STATE"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Notes"
+
+        binding.rvNotes.layoutManager = LinearLayoutManager(this)
+        binding.rvNotes.setHasFixedSize(true)
+        adapter = NoteAdapter(this)
+        binding.rvNotes.adapter = adapter
+
+        binding.fanAdd.setOnClickListener {
+            val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+            startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_ADD)
+        }
+
+        if (savedInstanceState == null) {
+            loadNotesAsync()
+        } else {
+            val list = savedInstanceState.getParcelableArrayList<Note>(EXTRA_STATE)
+            if (list != null) {
+                adapter.listNotes = list
+            }
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelableArrayList(EXTRA_STATE, adapter.listNotes)
+    }
+
+    private fun loadNotesAsync() {
+        GlobalScope.launch(Dispatchers.Main) {
+            binding.progressBar.visibility = View.VISIBLE
+            val noteHelper = NoteHelper.getInstance(applicationContext)
+            noteHelper.open()
+            val defferedNotes = async(Dispatchers.IO) {
+                val cursor = noteHelper.queryAll()
+                MappingHelper.mapCursorToArrayList(cursor)
+            }
+            binding.progressBar.visibility = View.INVISIBLE
+            val notes = defferedNotes.await()
+            if (notes.size > 0) {
+                adapter.listNotes = notes
+            } else {
+                adapter.listNotes = ArrayList()
+                showSnackBarMessage("There is no data")
+            }
+            noteHelper.close()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data != null) {
+            when (requestCode) {
+                NoteAddUpdateActivity.REQUEST_ADD -> if (resultCode == NoteAddUpdateActivity.RESULT_ADD) {
+                    val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE) as Note
+                    adapter.addItem(note)
+                    binding.rvNotes.smoothScrollToPosition(adapter.itemCount - 1)
+                    showSnackBarMessage("One item added")
+                }
+                NoteAddUpdateActivity.REQUEST_UPDATE ->
+                    when (resultCode) {
+                        NoteAddUpdateActivity.RESULT_UPDATE -> {
+                            val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE) as Note
+                            val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
+
+                            adapter.updateItem(position, note)
+                            binding.rvNotes.smoothScrollToPosition(position)
+
+                            showSnackBarMessage("One item updated")
+                        }
+                        NoteAddUpdateActivity.RESULT_DELETE -> {
+                            val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
+
+                            adapter.removeItem(position)
+
+                            showSnackBarMessage("One item deleted")
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun showSnackBarMessage(message: String) {
+        Snackbar.make(binding.rvNotes, message, Snackbar.LENGTH_SHORT).show()
+    }
+
+}
